d9321b75b4d2fb35ca27eb040100a1d8836040f2
SangUk Park <parksang1993@gmail.com>
2023-11-20 17:04:38 +0900

    Edit .gitignore (ignore 3D modeling)

---
+++ Tutorial.Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Tutorial.Landmarks
+//
+//  Created by 박상욱 on 11/20/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
